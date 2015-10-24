879f4eaab5bcb5efe9d653304c70d998797bf655
addtheletters <bzhang.2003@gmail.com>
2015-10-24 00:41:25 -0400

    visual note value display

--- music.js
+++ music.js
@@ -1,4 +1,19 @@
 
+var note_association = {
+	0:"C",
+	1:"C#",
+	2:"D",
+	3:"D#",
+	4:"E",
+	5:"F",
+	6:"F#",
+	7:"G",
+	8:"G#",
+	9:"A",
+	10:"A#",
+	11:"B"
+};
+
 var Note = function( index ){
 	this.num = index;
 };
