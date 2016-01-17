406d73d13a4c6e0da43373fe23255e118953d3b3
addtheletters <bzhang.2003@gmail.com>
2016-01-17 17:36:04 -0500

    nodes light up

--- js/sequencerealizer.js
+++ js/sequencerealizer.js
@@ -25,13 +25,15 @@ function renderNode( node, width, zI ){
         }
     }
     else if(node.value instanceof music.Note){
+        
         ret.classList.add("musicnote");
         ret.onclick = function(){
             console.log("Clicked a node. Wow. What a terrible person.");
             console.log(node.value);
             
             document.getElementById("infobox1").innerText = node.value;
-            playNote(0, node.value.num, 127, node.getDuration(), 0);
+            //playNote(0, node.value.num, 127, node.getDuration(), 0);
+            playNode(node, 0, 127);
         }
         ret.onmouseover = function(){
             ret.oldZI = ret.style.zIndex;
@@ -52,5 +54,8 @@ function renderNode( node, width, zI ){
         ret.appendChild(lbl2);
         ret.style.backgroundColor = colors.getStyleFromRGB(colors.HSVtoRGB(getIndicHSV(node.value)));
     }
+    
+    node.domElement = ret;
+    
     return ret;
 }
